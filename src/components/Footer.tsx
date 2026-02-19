5d109b694da22ec46aba4751a781576c3176557d
Mark Musumba <okiyomark01@gmail.com>
2026-02-19 10:52:47 +0300

    updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Page } from '../types';
 import { Facebook, Twitter, Instagram, Mail, Phone, MapPin } from 'lucide-react';
 
